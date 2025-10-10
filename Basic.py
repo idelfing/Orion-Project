4cc90ae8d4387305a5d8c484cca0def65604134b
Ivan Delfin Garcia <idelfing@liverpool.com.mx>
2025-10-10 10:21:48 -0600

    Ejemplo py
    Ejemplo para pull de py

---
+++ Basic.py
@@ -0,0 +1,17 @@
+user = int(input("Add your age: "))
+
+if (user > 18):
+    print("Ehhhh, si se pudo XD")
+elif (user < 18):
+    print("No se puede")
+else:
+    print("yei ")
+'''
+if (condicion):
+    # Codigo
+elif (condicion):
+    # Codigo
+else:
+    # Codigo
+'''
+
